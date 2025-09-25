5e86db22b5ae17be5ed1e1a2cd2db78855204c2d
jhonhader1983 <jhonhader1983@gmail.com>
2025-09-25 16:32:50 -0500

    fix: usar backend de vercel en calculadora

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import '../styles/Calculadora.css'
 import Resultado from "./Resultado";
 
+// URL del backend en Vercel
+const API_URL = "https://back-convertidor-vercel.vercel.app/api";
+
 function Calculadora(){
     const [number1, setNumber1] = useState('');
     const [number2, setNumber2] = useState('');
@@ -10,32 +13,32 @@ function Calculadora(){
     function handleSubmit(e){
         e.preventDefault();
         const operacion = e.target.value;
-        fetch(`http://localhost:3500/v1/calculadora/${operacion}`, {
+
+        fetch(`${API_URL}/calculadora/${operacion}`, {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({number1, number2})
+            body: JSON.stringify({ number1, number2 })
         })
-            .then(res =>res.json())
-            .then(responseData => {
-                setResultado(responseData.resultado)
-                // setResultado(responseData)
-                // console.log(resultado)
-            })
+        .then(res => res.json())
+        .then(responseData => {
+            setResultado(responseData.resultado);
+        })
+        .catch(err => console.error("Error en la calculadora:", err));
     }
 
     return (
         <div className="container">
             <h1 id="txtCalculadora">CALCULADORA</h1>
             <form>
-                <input type="text" className="number" onChange={(e)=>{setNumber1(e.target.value)}}/><br />
-                <input type="text" className="number" onChange={(e)=>{setNumber2(e.target.value)}}/><br />
+                <input type="text" className="number" onChange={(e)=> setNumber1(e.target.value)}/><br />
+                <input type="text" className="number" onChange={(e)=> setNumber2(e.target.value)}/><br />
                 <input type="submit" className="btnEnviar" value="sumar" onClick={handleSubmit}/>
                 <input type="submit" className="btnEnviar" value="restar" onClick={handleSubmit}/>
                 <input type="submit" className="btnEnviar" value="multiplicar" onClick={handleSubmit}/>
             </form>
-            <Resultado resultado={"El resultado es "+ resultado}/>
+            <Resultado resultado={"El resultado es " + resultado}/>
         </div>
     )
 }
 
-export default Calculadora
+export default Calculadora;
